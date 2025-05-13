f74359c954c53cc7a57786a4cde53086e00c3cba
Nathan Toups <n@rjrbt.io>
2025-05-12 22:08:54 -0600

    feat: mvp of working staging environment

--- projects/gnoland/scripts/entrypoint.sh
+++ projects/gnoland/scripts/entrypoint.sh
@@ -1,11 +1,31 @@
 #!/bin/bash
-
 set -e
 
-gnodev -v \
-    -web-listener 0.0.0.0:8888 \
-    -node-rpc-listener 0.0.0.0:26657 \
-    -chain-id labsnet1 \
-    -balance-file /gnoroot/gno.land/genesis/genesis_balances.txt &
-
-caddy run --config /etc/caddy/Caddyfile
+case "$1" in
+  dev)
+    # Run in development mode
+    echo "Starting in development mode..."
+    gnodev -v \
+      -web-listener 0.0.0.0:8888 \
+      -node-rpc-listener 0.0.0.0:26657 \
+      -balance-file /gnoroot/gno.land/genesis/genesis_balances.txt
+    ;;
+  
+  staging)
+    # Run in staging mode (the current production configuration)
+    echo "Starting in staging mode..."
+    gnodev staging -v \
+      -web-listener 0.0.0.0:8888 \
+      -node-rpc-listener 0.0.0.0:26657 \
+      -chain-id labsnet1 \
+      -balance-file /gnoroot/gno.land/genesis/genesis_balances.txt &
+    
+    caddy run --config /etc/caddy/Caddyfile
+    ;;
+  
+  *)
+    # Run whatever command is passed
+    echo "Running custom command:" "$@"
+    exec "$@"
+    ;;
+esac

e0bd05145f5292638a63a38efa5d419d0ada883d
Nathan Toups <n@rjrbt.io>
2025-05-15 12:18:24 -0600

    chore: update dockerfile with streamlined build and run

--- projects/gnoland/scripts/entrypoint.sh
+++ projects/gnoland/scripts/entrypoint.sh
@@ -8,7 +8,7 @@ case "$1" in
     gnodev -v \
       -web-listener 0.0.0.0:8888 \
       -node-rpc-listener 0.0.0.0:26657 \
-      -balance-file /gnoroot/gno.land/genesis/genesis_balances.txt
+      -balance-file /gnoroot/gno.land/genesis/balances_overlay.txt
     ;;
   
   staging)
@@ -18,7 +18,7 @@ case "$1" in
       -web-listener 0.0.0.0:8888 \
       -node-rpc-listener 0.0.0.0:26657 \
       -chain-id labsnet1 \
-      -balance-file /gnoroot/gno.land/genesis/genesis_balances.txt &
+      -balance-file /gnoroot/gno.land/genesis/balances_overlay.txt &
     
     caddy run --config /etc/caddy/Caddyfile
     ;;

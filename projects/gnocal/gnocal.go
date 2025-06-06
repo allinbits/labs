b011625d9fc09fc04ef29be466b42010cbefbff5
masonmcbride <mcbridedmason@gmail.com>
2025-06-05 22:09:34 -0700

    feat: func RenderCal(string path) string

--- projects/gnocal/gnocal.go
+++ projects/gnocal/gnocal.go
@@ -3,7 +3,9 @@ package gnocal
 // use chi v5 for server routing
 
 import (
+	"fmt"
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/gnolang/gno/gno.land/pkg/gnoclient"
@@ -11,6 +13,8 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
+
+var f = fmt.Sprintf
 
 type Server struct {
 	router    *chi.Mux
@@ -32,10 +36,10 @@ func NewServer() *Server {
 	s.router.Use(middleware.Recoverer)
 
 	s.router.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Welcome to Gnocal!"))
+		w.Write([]byte("navigate to /realm/{YOUR GNO.LAND REALM}:{OPTS} to get your calendar"))
 	})
 
-	s.router.Get("/realm/*", s.RenderRelay)
+	s.router.Get("/realm/*", s.RenderCalFromRealm)
 
 	return s
 }
@@ -44,53 +48,26 @@ func (s *Server) Run() error {
 	return http.ListenAndServe(":8080", s.router)
 }
 
-func (s *Server) RenderRelay(w http.ResponseWriter, r *http.Request) {
-	path := chi.URLParam(r, "*")
-	switch {
-	case strings.HasSuffix(path, ".ics"):
-		realmPath := strings.TrimSuffix(path, ".ics")
-		rawICS, _, err := s.gnoClient.QEval(realmPath, `ToICS()`)
-		if err != nil {
-			http.Error(w, "ToICS failed:%s"+err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		payload := extractString(rawICS)
-
-		//w.Header().Set("Content-Type", "text/calendar; charset=utf-8")
-		w.Write([]byte(strings.ReplaceAll(payload, `\n`, "\n")))
-		return
-
-	case strings.HasSuffix(path, ".json"):
-		realmPath := strings.TrimSuffix(path, ".json")
-		raw, _, err := s.gnoClient.QEval(realmPath, `ToJSON()`)
-		if err != nil {
-			http.Error(w, "ToJSON failed: "+err.Error(), http.StatusInternalServerError)
-			return
-		}
-		payload := extractString(raw)
-
-		w.Header().Set("Content-Type", "application/json; charset=utf-8")
-		w.Write([]byte(strings.ReplaceAll(payload, `\n`, "\n")))
-		return
-
-	case strings.HasSuffix(path, "/calendar"):
-		realmPath := path
-		raw, _, err := s.gnoClient.QEval(realmPath, `Render("")`)
-		if err != nil {
-			http.Error(w, "Render failed: "+err.Error(), http.StatusInternalServerError)
-			return
-		}
-		payload := extractString(raw)
-
-		w.Header().Set("Content-Type", "text/html; charset=utf-8")
-		w.Write([]byte(payload))
-		return
-
-	default:
-		http.NotFound(w, r)
+func (s *Server) RenderCalFromRealm(w http.ResponseWriter, r *http.Request) {
+	//gnocal.com/gno.land/r/buidlthefuture000/events/gnolandlaunch/calendar?format=ics
+	//gnocal.com/gno.land/r/buidlthefuture000/events/gnolandlaunch/calendar
+	calendarPath := chi.URLParam(r, "*")
+	if calendarPath == "" {
+		http.Error(w, "missing realm path", http.StatusBadRequest)
 		return
 	}
+
+	rawQuery := r.URL.RawQuery // e.g. "apple=2&session=1&session=100&session=0&format=json"
+
+	path := strconv.Quote("?" + rawQuery)
+
+	stringToken, _, err := s.gnoClient.QEval(calendarPath, f(`RenderCal(%s)`, path))
+	if err != nil {
+		http.Error(w, "QEval error: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Write([]byte(strings.ReplaceAll(extractString(stringToken), `\n`, "\n")))
 }
 
 func newGnoClient() (*gnoclient.Client, error) {
@@ -110,20 +87,3 @@ func extractString(s string) string {
 	s, _ = strings.CutSuffix(s, `" string)`)
 	return s
 }
-
-// NOTE: here are some ideas on how we could translate the URLS
-
-// RenderFeed() string
-
-// func RenderFeed(path string) string
-// RenderFeed("ics") string
-
-// gno.land/r/foo/event1
-// RenderFeed("ics?startdate=2023-10-01&enddate=2023-10-31")
-
-// gno.land/r/foo/event1
-
-//gnocal.com/f/gno.land/r/foo/event1.json?startdate=2023-10-01&enddate=2023-10-31
-
-// gno.land/r/foo/eventAggregator
-// RenderFeed("eventAggregator?startdate=2023-10-01&enddate=2023-10-31&topic=security")

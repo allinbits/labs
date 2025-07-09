6797506cbe3f21c4e1d046a4b2af06601d383aed
Nathan Toups <n@rjrbt.io>
2025-07-09 15:56:57 -0600

    chore: remove comments

--- projects/gnoquery/main.go
+++ projects/gnoquery/main.go
@@ -10,47 +10,38 @@ import (
 )
 
 func main() {
-	// Get default remote from environment or use default
 	defaultRemote := os.Getenv("GNOQUERY_REMOTE")
 	if defaultRemote == "" {
 		defaultRemote = "tcp://0.0.0.0:26657"
 	}
 
-	// Define flags
 	remote := flag.String("remote", defaultRemote, "Remote node URL (can also be set via GNOQUERY_REMOTE env var)")
 	setFlagUsage(os.Args[0])
 	flag.Parse()
 
-	// Check for required arguments
 	if flag.NArg() < 2 {
 		flag.Usage()
 		os.Exit(1)
 	}
 
-	// Get arguments
-	realmPath := flag.Arg(0)
-	functionCall := flag.Arg(1)
-
-	// Create RPC client
 	rpcClient, err := rpcclient.NewHTTPClient(*remote)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error creating RPC client: %v\n", err)
 		os.Exit(1)
 	}
-
-	// Create Gno client
 	client := gnoclient.Client{
 		RPCClient: rpcClient,
 	}
 
-	// Execute query
+	realmPath := flag.Arg(0)
+	functionCall := flag.Arg(1)
+
 	result, _, err := client.QEval(realmPath, functionCall)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Error executing query: %v\n", err)
 		os.Exit(1)
 	}
 
-	// Print result
 	fmt.Println(result)
 }
 
